5b461b845b7e55f50b95c745ee3c779de8aaa0f1
Blaise Laflamme <blaise@laflamme.org>
2010-11-28 00:18:45 -0500

    Updated Pylons pygment styles

--- pylons_theme_support.py
+++ pylons_theme_support.py
@@ -6,7 +6,7 @@ from pygments.token import Keyword, Name, Comment, String, Error, \
 
 class PylonsStyle(Style):
     """
-    Pylons pygments style based on tango style
+    Pylons pygments style based on friendly style
     """
 
     # work in progress...
@@ -15,88 +15,50 @@ class PylonsStyle(Style):
     default_style = ""
 
     styles = {
-        # No corresponding class for the following:
-        #Text:                     "", # class:  ''
-        Whitespace:                "underline #f8f8f8",      # class: 'w'
-        Error:                     "#a40000 bg:#fbe3e4", # class: 'err'
-        Other:                     "#000000",                # class 'x'
+        Whitespace:                "#bbbbbb",
+        Comment:                   "italic #60a0b0",
+        Comment.Preproc:           "noitalic #007020",
+        Comment.Special:           "noitalic bg:#fff0f0",
 
-        Comment:                   "italic #8f5902", # class: 'c'
-        Comment.Multiline:         "italic #8f5902", # class: 'cm'
-        Comment.Preproc:           "italic #8f5902", # class: 'cp'
-        Comment.Single:            "italic #8f5902", # class: 'c1'
-        Comment.Special:           "italic #8f5902", # class: 'cs'
+        Keyword:                   "bold #007020",
+        Keyword.Pseudo:            "nobold",
+        Keyword.Type:              "nobold #902000",
 
-        Keyword:                   "bold #204a87",   # class: 'k'
-        Keyword.Constant:          "bold #204a87",   # class: 'kc'
-        Keyword.Declaration:       "bold #204a87",   # class: 'kd'
-        Keyword.Namespace:         "bold #204a87",   # class: 'kn'
-        Keyword.Pseudo:            "bold #204a87",   # class: 'kp'
-        Keyword.Reserved:          "bold #204a87",   # class: 'kr'
-        Keyword.Type:              "bold #204a87",   # class: 'kt'
+        Operator:                  "#666666",
+        Operator.Word:             "bold #007020",
 
-        Operator:                  "bold #ce5c00",   # class: 'o'
-        Operator.Word:             "bold #204a87",   # class: 'ow' - like keywords
+        Name.Builtin:              "#007020",
+        Name.Function:             "#06287e",
+        Name.Class:                "bold #0e84b5",
+        Name.Namespace:            "bold #0e84b5",
+        Name.Exception:            "#007020",
+        Name.Variable:             "#bb60d5",
+        Name.Constant:             "#60add5",
+        Name.Label:                "bold #002070",
+        Name.Entity:               "bold #d55537",
+        Name.Attribute:            "#0e84b5",
+        Name.Tag:                  "bold #062873",
+        Name.Decorator:            "bold #555555",
 
-        Punctuation:               "bold #000000",   # class: 'p'
+        String:                    "#4070a0",
+        String.Doc:                "italic",
+        String.Interpol:           "italic #70a0d0",
+        String.Escape:             "bold #4070a0",
+        String.Regex:              "#235388",
+        String.Symbol:             "#517918",
+        String.Other:              "#c65d09",
+        Number:                    "#40a070",
 
-        # because special names such as Name.Class, Name.Function, etc.
-        # are not recognized as such later in the parsing, we choose them
-        # to look the same as ordinary variables.
-        Name:                      "#000000",        # class: 'n'
-        Name.Attribute:            "#c4a000",        # class: 'na' - to be revised
-        Name.Builtin:              "#204a87",        # class: 'nb'
-        Name.Builtin.Pseudo:       "#3465a4",        # class: 'bp'
-        Name.Class:                "#000000",        # class: 'nc' - to be revised
-        Name.Constant:             "#000000",        # class: 'no' - to be revised
-        Name.Decorator:            "bold #5c35cc",   # class: 'nd' - to be revised
-        Name.Entity:               "#ce5c00",        # class: 'ni'
-        Name.Exception:            "bold #cc0000",   # class: 'ne'
-        Name.Function:             "#000000",        # class: 'nf'
-        Name.Property:             "#000000",        # class: 'py'
-        Name.Label:                "#f57900",        # class: 'nl'
-        Name.Namespace:            "#000000",        # class: 'nn' - to be revised
-        Name.Other:                "#000000",        # class: 'nx'
-        Name.Tag:                  "bold #204a87",   # class: 'nt' - like a keyword
-        Name.Variable:             "#000000",        # class: 'nv' - to be revised
-        Name.Variable.Class:       "#000000",        # class: 'vc' - to be revised
-        Name.Variable.Global:      "#000000",        # class: 'vg' - to be revised
-        Name.Variable.Instance:    "#000000",        # class: 'vi' - to be revised
+        Generic.Heading:           "bold #000080",
+        Generic.Subheading:        "bold #800080",
+        Generic.Deleted:           "#A00000",
+        Generic.Inserted:          "#00A000",
+        Generic.Error:             "#FF0000",
+        Generic.Emph:              "italic",
+        Generic.Strong:            "bold",
+        Generic.Prompt:            "bold #c65d09",
+        Generic.Output:            "#888",
+        Generic.Traceback:         "#04D",
 
-        # since the tango light blue does not show up well in text, we choose
-        # a pure blue instead.
-        Number:                    "bold #0000cf",   # class: 'm'
-        Number.Float:              "bold #0000cf",   # class: 'mf'
-        Number.Hex:                "bold #0000cf",   # class: 'mh'
-        Number.Integer:            "bold #0000cf",   # class: 'mi'
-        Number.Integer.Long:       "bold #0000cf",   # class: 'il'
-        Number.Oct:                "bold #0000cf",   # class: 'mo'
-
-        Literal:                   "#000000",        # class: 'l'
-        Literal.Date:              "#000000",        # class: 'ld'
-
-        String:                    "#4e9a06",        # class: 's'
-        String.Backtick:           "#4e9a06",        # class: 'sb'
-        String.Char:               "#4e9a06",        # class: 'sc'
-        String.Doc:                "italic #8f5902", # class: 'sd' - like a comment
-        String.Double:             "#4e9a06",        # class: 's2'
-        String.Escape:             "#4e9a06",        # class: 'se'
-        String.Heredoc:            "#4e9a06",        # class: 'sh'
-        String.Interpol:           "#4e9a06",        # class: 'si'
-        String.Other:              "#4e9a06",        # class: 'sx'
-        String.Regex:              "#4e9a06",        # class: 'sr'
-        String.Single:             "#4e9a06",        # class: 's1'
-        String.Symbol:             "#4e9a06",        # class: 'ss'
-
-        Generic:                   "#000000",        # class: 'g'
-        Generic.Deleted:           "#a40000",        # class: 'gd'
-        Generic.Emph:              "italic #000000", # class: 'ge'
-        Generic.Error:             "#ef2929",        # class: 'gr'
-        Generic.Heading:           "bold #000080",   # class: 'gh'
-        Generic.Inserted:          "#00A000",        # class: 'gi'
-        Generic.Output:            "italic #000000", # class: 'go'
-        Generic.Prompt:            "#8f5902",        # class: 'gp'
-        Generic.Strong:            "bold #000000",   # class: 'gs'
-        Generic.Subheading:        "bold #800080",   # class: 'gu'
-        Generic.Traceback:         "bold #a40000",   # class: 'gt'
+        Error:                     "#a40000 bg:#fbe3e4"
     }
